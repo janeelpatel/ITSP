191ca43e41d13495841869af1ba2e9ef5cfaeae4
janeelpatel <52424262+janeelpatel@users.noreply.github.com>
2019-07-02 02:30:12 +0530

    Create angle_calc.py

---
+++ angle_calc.py
@@ -0,0 +1,13 @@
+"""
+Created by : Team Unique Tech for ITSP, IIT Bombay
+
+The below code is for calculating the angle through which the bot needs to rotate in order to reach to the source. 
+The code extracts the required data and then using the principles similar to YDSE, calculates the rotation angle.
+
+"""
+
+import numpy as np
+import time
+from math import pi
+from data_processing import max_time_array
+
