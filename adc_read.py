ab7e55327022c237344713852c065009236c7517
janeelpatel <52424262+janeelpatel@users.noreply.github.com>
2019-07-01 23:43:48 +0530

    Create adc_read.py

---
+++ adc_read.py
@@ -0,0 +1,27 @@
+# Simple example of reading the MCP3008 analog input channels and printing
+# them all out.
+# Author: Tony DiCola
+# License: Public Domain
+import time
+
+# Import SPI library (for hardware SPI) and MCP3008 library.
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MCP3008
+
+
+# Software SPI configuration:
+# CLK  = 18
+# MISO = 23
+# MOSI = 24
+# CS   = 25
+# mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
+
+# Hardware SPI configuration:
+# SPI_PORT   = 0
+# SPI_DEVICE = 0
+# mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+
+# At this point you must have specified the config.
+
+def read_val(port): # port specifies the port to be read from
+	return mcp.read_adc(port) # 10-bit data reading from port
