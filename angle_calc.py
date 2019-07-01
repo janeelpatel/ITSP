39716a930fd074777cee5bcb995bf41350cc455b
janeelpatel <52424262+janeelpatel@users.noreply.github.com>
2019-07-02 03:19:45 +0530

    Update angle_calc.py

--- angle_calc.py
+++ angle_calc.py
@@ -8,6 +8,27 @@ The code extracts the required data and then using the principles similar to YDS
 
 import numpy as np
 import time
-from math import pi
-from data_processing import max_time_array
+import math
+from data_processing import max_time_array as mta
 
+dist = 0.05 # distance between mic in metres  
+sound_speed = 343 # speed of sound in m/s 
+
+time_diff_01 = np.abs(mta[0]-mta[1])
+time_diff_23 = np.abs(mta[2]-mta[3])
+
+dist_01 = time_diff_01 * sound_speed
+dist_23 = time_diff_23 * sound_speed
+
+angle_01 = atan2(dist_01, dist) # in radians
+angle_23 = atan2(dist_23, dist) # in radians
+
+if (mta[0]<mta[1]): # checks which mic is near the source
+	orientation_01 = 'C'
+else:
+	orientation_01 = 'AC'
+	
+# the orientation is fixed now
+# the sound waves are assumed to be planar 
+
+angle = (angle_01 + angle_23)/2 # average rotation angle in radians 
