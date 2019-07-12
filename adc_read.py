178270a48c4a10df96a9a3cbd9647d8010bc9ad9
janeelpatel <52424262+janeelpatel@users.noreply.github.com>
2019-07-12 15:23:49 +0530

    Update adc_read.py

--- adc_read.py
+++ adc_read.py
@@ -17,11 +17,9 @@ import Adafruit_MCP3008
 # mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
 
 # Hardware SPI configuration:
-# SPI_PORT   = 0
-# SPI_DEVICE = 0
-# mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
-
-# At this point you must have specified the config.
+SPI_PORT   = 0
+SPI_DEVICE = 0
+mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
 def read_val(): # port specifies the port to be read from
 	return mcp.read_adc(0), mcp.read_adc(1), mcp.read_adc(2), mcp.read_adc(3) # 10-bit data reading from port
