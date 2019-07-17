8c160eefde14cefed899c425039ab2d73914c40e
KalpeshRathi <51653533+KalpeshRathi@users.noreply.github.com>
2019-07-17 15:16:30 +0530

    2 mics

--- angle_calc.py
+++ angle_calc.py
@@ -15,13 +15,10 @@ dist = 0.05 # distance between mic in metres
 sound_speed = 343 # speed of sound in m/s 
 
 time_diff_01 = np.abs(mta[0]-mta[1])
-time_diff_23 = np.abs(mta[2]-mta[3])
 
 dist_01 = time_diff_01 * sound_speed
-dist_23 = time_diff_23 * sound_speed
 
 angle_01 = atan2(dist_01, dist) # in radians
-angle_23 = atan2(dist_23, dist) # in radians
 
 if (mta[0]<mta[1]): # checks which mic is near the source
 	orientation = 'C'
@@ -31,5 +28,5 @@ else:
 # the orientation is fixed now
 # the sound waves are assumed to be planar 
 
-angle_rad = (angle_01 + angle_23)/2 # average rotation angle in radians 
+angle_rad = angle_01 # average rotation angle in radians 
 angle_deg = angle_rad*180/(math.pi) # average rotation angle in degrees 
